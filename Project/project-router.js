c2ba09c051e558ce9252f5e968ede42d8332022e
Annette Odingo <odingoannette@gmail.com>
2020-01-17 09:02:40 +0000

    Add an endpoint that adds a project to project DB

--- Project/project-router.js
+++ Project/project-router.js
@@ -1,18 +1,28 @@
 const express = require('express');
-const projectDB = require('../data/helpers/projectModel');
+const ProjectDB = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    projectDB.get()
+router.get('/:id', (req, res)=>{
+    const {id}= req.params;
+    ProjectDB.get(id)
     .then(projects=>{
-        console.log(projects);
         res.status(200).json(projects);
     })
     .catch(error=>{
         res.status(500).json({error: "The projects couldn't be retrieved"})
     })
     
+})
+router.post('/', (req,res)=>{
+    const body= req.body;
+    ProjectDB.insert(body)
+    .then(project=>{
+        res.status(201).json(project)
+    })
+    .catch()
+    
+
 })
 
 module.exports= router;
