bbcc7827774f867d8cbd37fc79a6ee9b1a1e0295
Annette Odingo <odingoannette@gmail.com>
2020-01-17 09:09:33 +0000

    Add an endpoint that modifies project details

--- Project/project-router.js
+++ Project/project-router.js
@@ -6,11 +6,11 @@ const router = express.Router();
 router.get('/:id', (req, res)=>{
     const {id}= req.params;
     ProjectDB.get(id)
-    .then(projects=>{
-        res.status(200).json(projects);
+    .then(project=>{
+        res.status(200).json(project);
     })
     .catch(error=>{
-        res.status(500).json({error: "The projects couldn't be retrieved"})
+        res.status(500).json({error: "The project couldn't be retrieved"})
     })
     
 })
@@ -21,8 +21,17 @@ router.post('/', (req,res)=>{
         res.status(201).json(project)
     })
     .catch()
-    
-
+})
+router.put('/:id', (req,res)=>{
+    const body = req.body;
+    const {id} = req.params;
+    ProjectDB.update(id, body)
+    .then(project=>{
+        res.status(200).json(project);
+    })
+    .catch(error=>{
+        res.status(500).json({ error: "The project information could not be modified." }) 
+    })
 })
 
 module.exports= router;
